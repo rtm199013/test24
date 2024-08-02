f1e8eacbf48e34fdf1b6f6ac8adfda5b24c03d0e
mon <rtm199013@gmail.com>
2024-08-02 11:53:37 +0800

    1st hello word

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
+import HelloWord from "./components/HelloWord";
+
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      test
+      <HelloWord />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/HelloWord.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class HelloWord extends Component {
+  render() {
+    return <div>Hello Word - Testing</div>;
+  }
+}
+
+export default HelloWord;
